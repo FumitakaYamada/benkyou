f5ff24234b7da246ae50e339fb6b085375e55936
andpdas <andpdas@gmail.com>
2013-02-06 18:53:19 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Benkyou::Application.config.secret_token = '1ea64c0dcf63cdff2762587eb44995c2d7dd771e8e0d088222680d8df131366ccfa4daa8d8dd2044461064e2ce6ceb672ffa403b578810673d9485fdefd631aa'
